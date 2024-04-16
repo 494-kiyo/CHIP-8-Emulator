422b2c52aeb8cc3c06f1cde00bb9e0050b7cb4f9
Esther <estheryoo118@gmail.com>
2024-04-16 19:43:35 -0400

    update

--- chip8.c
+++ chip8.c
@@ -1,5 +1,27 @@
+#include<stdio.h>
+#include <stdint.h>
+
 #define MEMORY_SIZE 4096
 #define MEMORY_START 0x200
 
-uint8_t registers[16]{};
-uint8_t 
+typedef struct chip8 {
+    uint8_t registers[16]{};
+    uint8_t SP = 0xfa0;
+    uint16_t IR;
+    uint16_t PC = 0x200;
+    uint16_t stack[16]{};
+    uint8_t delay;
+    uint8_t sound;
+    uint8_t *screen = 0xF00;
+} chip8;
+
+void chip8::emulateCycle() {
+    uint16_t opcode;
+
+
+}
+
+int main() {
+    printf("hello");
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,1 +1,47 @@
-#include chip8.c
+#include<stdio.h>
+#include <stdint.h>
+
+#define MEMORY_SIZE 4096
+#define MEMORY_START 0x200
+
+#define OPCODE_X(opcode) ((opcode) & 0xF)
+
+uint8_t registers[16]; // 16 general purpose registers
+uint8_t SP; // stack pointer
+uint16_t IR; // current address
+uint16_t PC = 0x200; // next address
+uint16_t stack[16]; // stack
+uint16_t memory[MEMORY_SIZE];
+uint8_t delay;
+uint8_t sound;
+uint8_t screen;
+
+void loadFile () {
+    FILE* file = fopen("PONG.ch8", "rb");
+    if (!file) {
+        fprintf(stderr, "Error: Could not open file\n");
+        return;
+    }
+
+    // file size
+    fseek(file, 0, SEEK_END); // file pointer to the end
+    size_t file_size = ftell(file); // get the position
+    fseek(file, 0, SEEK_SET); // return to the starting position
+
+    size_t result = fread(memory + 0x200, 1, sizeof(memory) - 0x200, file);
+}
+
+void emulateCycle() {
+    uint16_t opcode = memory[PC]; // why 16? 
+    PC += 2;
+    printf("Opcode: 0x%04X\n", opcode);
+
+    // extraction
+
+}
+
+int main() {
+    loadFile();
+    emulateCycle();
+    return 0;
+}
